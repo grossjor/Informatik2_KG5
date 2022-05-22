b654c24327214f798d83c75119a8081dd29becbf
grossjor <jorik.gross@stud.hawk.de>
2022-05-22 13:01:50 +0200

    fahrrad

--- Transportmittel/transportmittel.h
+++ Transportmittel/transportmittel.h
@@ -6,13 +6,15 @@ using namespace std;
 class Transportmittel
 {
 private:
-  double hoehe=1;
-  double breite=2;
+  double hoehe;
+  double breite;
 public:
   //double* sethoehe {&hoehe};
   Transportmittel () {}
   Transportmittel (double hoehe, double breite)
   {
+    hoehe=4;
+    breite=5;
   }
   double gethoehe() {return hoehe;}
   double getbreite() {return breite;}
@@ -25,22 +27,45 @@ public:
 
 };
 class LandTransportmittel : public Transportmittel{
-private:
-  int radzahl=4;
-public:
-  double getkm() {return km};
+  private:
+    int radzahl=4;
+  public:
+    double km;
 
   LandTransportmittel(){
     }
-  LandTransportmittel(int radzahl,double hoehe,double breite) {
+  LandTransportmittel(int radzahl,double hoehe,double breite)
+    {
+      radzahl = 4;
+      breite = 2;
+      hoehe = 1;
     }
   void fahren(double km){
     }
   void schieben(double km){
     }
+      double getkm() {return km;}
 };
 class WasserTransportmittel : public Transportmittel{
+  private:
+    double bruttoregistertonnen;
   public :
-
-
+    WasserTransportmittel() {
+      }
+    WasserTransportmittel(double hoehe,double breite, double bruttoregistertonen) {
+      }
+};
+class Fahrrad : public LandTransportmittel{
+  private:
+    int anzahlgaenge;
+  public:
+    void freihaendigfahren (string name){
+      }
+    int getgaenge() {return anzahlgaenge;}
+    void schieben(double km){
+      }
+    Fahrrad(){
+      }
+    Fahrrad(double hoehe, double breite, int radzahl, int anzahlgaenge){
+      }
 };
